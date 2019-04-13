ac5514b667fadf498163b61b03944e64e585a0b6
Joshua Alpert <joshua.alpert1@gmail.com>
2019-04-13 16:18:35 -0700

    Added start of the insanity of helper functions for making the maze

--- RogueTilt/Assets/Scripts/LevelManeger.cs
+++ RogueTilt/Assets/Scripts/LevelManeger.cs
@@ -84,7 +84,42 @@ public class LevelManeger : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    void InitialiseMaze()
+    {
+        List<Vector3> TilesVisted = new List<Vector3>();
+        Vector3 CurrentTile =  startTile.GetPosition();
+
+        while (CurrentTile != exitTile.GetPosition())
+        {
+            //Add our current tile to where we have been
+            TilesVisted.Add(CurrentTile);
             
+            if (CurrentTile != startTile.GetPosition())
+            {
+                //Add the tile
+                floorTiles.Add(new Tile(CurrentTile));
+            }
+
+            List<Vector3> NeighborTiles = new List<Vector3>();
+
+
+
+            //Remove all tiles we have visited
+            foreach (Vector3 Tile in TilesVisted)
+            {
+                if(NeighborTiles.Contains(Tile))
+                {
+                    NeighborTiles.Remove(Tile);
+                }
+                
+            }
+
+
+            //Set the next tile we are going to look at
+            CurrentTile = NeighborTiles[Random.Range(0, NeighborTiles.Count - 1)];
+        }
 
     }
 
@@ -93,6 +128,7 @@ public class LevelManeger : MonoBehaviour
     // Init the board
     void Start()
     {
+        //Set up the Tiles
         InitialiseList();
 
         for(int i =0; i < floorTiles.Count; i++)
@@ -100,11 +136,58 @@ public class LevelManeger : MonoBehaviour
             Debug.Log("Tile at " + i + "Has the position: " + floorTiles[i].position.x + floorTiles[i].position.y);
         }
 
-        //Assign the center tile as the start tile
+        //Set pup the maze
+        InitialiseMaze();
 
         //Set the active Tile as the start tile
-        
+
         activeTile = startTile;
+    }
+
+
+    List<Vector3> GetNeighbours(Tile tile_in)
+    {
+        List<Vector3> Neighbourhood = new List<Vector3>();
+        List<Vector3> AllVectors = GetAllVectors();
+
+        Vector3 NeighbourNorth = new Vector3(tile_in.GetPosition().x, tile_in.GetPosition().y -1, 0);
+        Vector3 NeighbourEast = new Vector3(tile_in.GetPosition().x + 1, tile_in.GetPosition().y, 0);
+        Vector3 NeighbourSouth = new Vector3(tile_in.GetPosition().x, tile_in.GetPosition().y + 1, 0);
+        Vector3 NeighbourWest = new Vector3(tile_in.GetPosition().x - 1, tile_in.GetPosition().y, 0);
+
+        AllVectors.Remove(startTile.GetPosition());
+
+        if (AllVectors.Contains(NeighbourNorth))
+        {
+            Neighbourhood.Add(NeighbourNorth);
+        }
+        if (AllVectors.Contains(NeighbourEast))
+        {
+            Neighbourhood.Add(NeighbourEast);
+        }
+        if (AllVectors.Contains(NeighbourSouth))
+        {
+            Neighbourhood.Add(NeighbourSouth);
+        }
+        if (AllVectors.Contains(NeighbourWest))
+        {
+            Neighbourhood.Add(NeighbourWest);
+        }
+
+
+        return Neighbourhood;
+    }
+
+    List<Vector3> GetAllVectors()
+    {
+        List<Vector3> all_vectors = new List<Vector3>();
+
+        foreach(Tile tile in floorTiles)
+        {
+            all_vectors.Add(tile.GetPosition());
+        }
+
+        return all_vectors;
     }
 
     // Update is called once per frame
