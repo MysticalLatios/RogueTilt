070e336fe523954109efa346f39e127caf1359b5
Joshua Alpert <joshua.alpert1@gmail.com>
2019-04-13 12:32:41 -0700

    added start of level manager

--- RogueTilt/Assets/Scripts/LevelManeger.cs
+++ RogueTilt/Assets/Scripts/LevelManeger.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class LevelManeger : MonoBehaviour
 {
+    // Vars for our Level
+    public int columns = 6;
+    public int rows = 6;
+    public GameObject exit;
+    public GameObject[] floorTiles;
+
+    //Possible grid positions
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+
     // Start is called before the first frame update
+    // Init the board
     void Start()
     {
         
