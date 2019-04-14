83175310938d4a98799162b065d8b1375b4e92e5
Zach Freeman <zfreeman997@gmail.com>
2019-04-13 18:16:58 -0700

    Progress toward spawning tiles

--- RogueTilt/Assets/Scripts/Tile.cs
+++ RogueTilt/Assets/Scripts/Tile.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    private GameObject baseTile;
+    private GameObject baseTile = null;
     //public List<GameObject> enemies;
     public List<GameObject> tilePrefabs;
     //Where the tile is on the grid
     public Vector3 position;
     private int offset = 15;
+
+
+
+    private void Start()
+    {
+        
+    }
 
     public void SetPosition(Vector3 pos_in)
     {
@@ -24,14 +31,16 @@ public class Tile : MonoBehaviour
     private void PlaceSelf()
     {
         //Assign it's tile type from the list of prefabs
+      
         baseTile = tilePrefabs[0];
-
+        
+ 
         //place according to it's postion
         //Each tile is 26 X 26
         //The center is the position because that's how scale be, we need to, at the very least off set each tile by 13
         //We considered the idea that girds should be 30 x 30, so offset by 15 for placement
 
-        Instantiate(baseTile, new Vector3((position.x + 1) * offset, 0, (position.z + 1) * offset), Quaternion.identity);
+        Instantiate(baseTile, new Vector3((position.x + 1) * offset, 0, (position.z + 1) * offset), new Quaternion(0, 0, 0, 0));
 
     }
 
@@ -39,11 +48,14 @@ public class Tile : MonoBehaviour
     public Tile(Vector3 position_in)
     {
         //Assign our baseTile from a list of prefabs
-
+        Debug.Log("Got to tile");
         //Randomly assign the enemies
-
         //Set the position
         SetPosition(position_in);
+        if (tilePrefabs.Count > 0)
+        {
+            PlaceSelf();
+        }
     }
 
     public Tile(Vector3 position_in, string tile_name)
@@ -51,9 +63,13 @@ public class Tile : MonoBehaviour
         //Assign our baseTile based on the tile_name string
 
         //hard coded to pick base tile currently
-
+        Debug.Log("Got to tile");
         //Set the position in the grid 
         SetPosition(position_in);
+        if (tilePrefabs.Count > 0)
+        {
+            PlaceSelf();
+        }
     }
 
 
