da57dfd8a691a872c66c5df6b988927825c62843
Zach Freeman <zfreeman997@gmail.com>
2019-04-14 12:54:34 -0700

    Player detects it's collisions with door

--- RogueTilt/Assets/Scripts/Player.cs
+++ RogueTilt/Assets/Scripts/Player.cs
@@ -4,10 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float timetoTele = 0.25f;
+    private float stayCount = 0.0f;
+    bool enter_door = false;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Found Something");
+        if (other.tag == "Door")
+        {
+            enter_door = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(enter_door)
+        {
+            Debug.Log("In door");
+            if (stayCount > timetoTele)
+            {
+
+            }
+            else
+            {
+                stayCount += Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Left Door");
+        if (other.tag == "Door")
+        {
+            enter_door = false;
+        }
+        stayCount = 0;
     }
 
     // Update is called once per frame
