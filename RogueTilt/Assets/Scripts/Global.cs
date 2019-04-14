49be53fc829b915f6b28f6f3c2a6209186755182
Joshua Alpert <joshua.alpert1@gmail.com>
2019-04-13 22:44:45 -0700

    Added the global script

---
+++ RogueTilt/Assets/Scripts/Global.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    public static GameObject ActiveTile;
+
+    public static GameObject GetActiveTile()
+    {
+        return ActiveTile;
+    }
+
+    public void SetActiveTile(GameObject gameObject)
+    {
+        ActiveTile = gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
