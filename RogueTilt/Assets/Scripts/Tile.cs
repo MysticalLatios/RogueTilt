0b40a6d628a25429a987dfeae6610cd907bfa0e6
Zach Freeman <zfreeman997@gmail.com>
2019-04-13 17:46:51 -0700

    Added what I think will make tiles spawn doesn't run yet

--- RogueTilt/Assets/Scripts/Tile.cs
+++ RogueTilt/Assets/Scripts/Tile.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile
+public class Tile : MonoBehaviour
 {
-   //public GameObject baseTile;
-   //public List<GameObject> enemies;
-
+    private GameObject baseTile;
+    //public List<GameObject> enemies;
+    public List<GameObject> tilePrefabs;
     //Where the tile is on the grid
     public Vector3 position;
+    private int offset = 15;
 
     public void SetPosition(Vector3 pos_in)
     {
@@ -18,6 +19,20 @@ public class Tile
     public Vector3 GetPosition()
     {
         return position;
+    }
+
+    private void PlaceSelf()
+    {
+        //Assign it's tile type from the list of prefabs
+        baseTile = tilePrefabs[0];
+
+        //place according to it's postion
+        //Each tile is 26 X 26
+        //The center is the position because that's how scale be, we need to, at the very least off set each tile by 13
+        //We considered the idea that girds should be 30 x 30, so offset by 15 for placement
+
+        Instantiate(baseTile, new Vector3((position.x + 1) * offset, 0, (position.z + 1) * offset), Quaternion.identity);
+
     }
 
     //Tile constructor
@@ -35,9 +50,9 @@ public class Tile
     {
         //Assign our baseTile based on the tile_name string
 
-        //Randomly assign the enemies
+        //hard coded to pick base tile currently
 
-        //Set the position
+        //Set the position in the grid 
         SetPosition(position_in);
     }
 
