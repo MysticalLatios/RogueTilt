af885f8319c9c0eb94c61f604ae5ebc32f868588
jasmin <jm3625@humboldt.edu>
2019-04-17 15:56:56 -0700

    #1
    UI basic

---
+++ RogueTilt/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    //opening scene to play
+    public void PlayGame() => SceneManager.LoadScene("TileCreation");
+    
+    //quiting game
+    public void QuitGame() => Application.Quit();
+}
