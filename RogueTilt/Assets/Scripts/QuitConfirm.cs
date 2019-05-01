b01468ba4facd6020679762e976af1094d6af6d4
jasmin <jm3625@humboldt.edu>
2019-05-01 12:39:35 -0700

    Improved UI #9
    improved ui layout and added a quit page

---
+++ RogueTilt/Assets/Scripts/QuitConfirm.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitConfirm : MonoBehaviour
+{
+    //quiting game
+    public void QuitGame() => Application.Quit();
+}
